8b3579fe47bbd03e82870afe4fec6bdc4685ae42
Deolindo Baptista <contact@deolindobaptista.com>
2024-08-07 09:13:25 +0200

    Creation of Google Authentication via firebase and setup google auth on the server

---
+++ Portfolio-frontend/src/common/firebase.jsx
@@ -0,0 +1,38 @@
+// PersonalWebSite\Portfolio-frontend\src\common\firebase.jsx
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// add a google auth
+
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth();
+
+export const authWithGoogle = async () => {
+  let user = null;
+
+  await signInWithPopup(auth, provider)
+    .then((result) => {
+      user = result.user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return user;
+};
