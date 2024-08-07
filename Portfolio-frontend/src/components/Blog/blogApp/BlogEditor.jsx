3421590a86659710c7dc31e138d9f0f448418962
Deolindo Baptista <contact@deolindobaptista.com>
2024-08-07 15:15:10 +0200

    In the blog -  The Blog Editor - Navbar •	creation of Publish and Save Draft buttons and link in the form of an icon to reference the page / blog
     On branch main
    
     Changes to be committed:
    	modified:   Portfolio-frontend/src/components/About/AboutMeWelcomeContainer.scss
    	modified:   Portfolio-frontend/src/components/Blog/Pages/BlogUserAuthFormPage.scss
    	modified:   Portfolio-frontend/src/components/Blog/blogApp/BlogEditor.jsx
    	modified:   Portfolio-frontend/src/components/Blog/blogApp/BlogEditor.scss
    	modified:   Portfolio-frontend/src/components/Blog/blogApp/BlogNavbar.scss
    	modified:   Portfolio-frontend/src/components/Projects/ProjectsMain.scss

--- Portfolio-frontend/src/components/Blog/blogApp/BlogEditor.jsx
+++ Portfolio-frontend/src/components/Blog/blogApp/BlogEditor.jsx
@@ -1,14 +1,27 @@
 // PersonalWebSite\Portfolio-frontend\src\components\Blog\blogApp\BlogEditor.jsx
 
+import { Link } from 'react-router-dom';
+import images from '../../../assets/imageIndex.js';
 import './BlogEditor.scss';
 
 export const BlogEditor = () => {
   return (
     <>
-      <div className='blogEditor'>
-        <h2>Blog Editor</h2>
-        {/* Add your editor component here */}
-      </div>
+      <nav className='blogEditorNavbar'>
+        {/* Link to the blog homepage */}
+        <Link to='/blog'>
+          <img src={images.deoIconGold} alt='Circular logo with the letter D' />
+        </Link>
+        <p className='blogEditorTitle'>New blog</p>
+        <div className='blogEditorNavbarButtons'>
+          <button className='blogEditorNavbarButton'>Publish</button>
+          <button className='blogEditorNavbarButton blogEditorNavbarButtonDraft'>
+            save draft
+          </button>
+        </div>
+      </nav>
+
+      <div className='blogEditor'></div>
     </>
   );
 };
